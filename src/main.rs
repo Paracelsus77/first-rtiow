69e88492580f3ee02b1693acc8fee6a6ce6bdbd2
janne <jan.valtanen@gmail.com>
2025-12-25 12:03:23 +0100

    first image from ray tracing in one weekend

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use minifb::{Key, ScaleMode, Window, WindowOptions};
+
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
+
+fn main() {
+    let mut buffer = vec![0u32; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "first program rtiow",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: true,
+            scale_mode: ScaleMode::UpperLeft,
+            ..WindowOptions::default()
+        },
+    )
+    .expect("Unable to create window");
+
+    window.set_target_fps(100);
+
+    let mut size = (0, 0);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let new_size = window.get_size();
+        if new_size != size {
+            size = new_size;
+            buffer.resize(size.0 * size.1, 0);
+        }
+
+        for j in 0..HEIGHT {
+            for i in 0..WIDTH {
+                let r = i as f64 / (WIDTH - 1) as f64;
+                let g = j as f64 / (HEIGHT - 1) as f64;
+                let b = 0.0;
+
+                let ir = (r * 255.999) as u32;
+                let ig = (g * 255.999) as u32;
+                let ib = (b * 255.999) as u32;
+
+                buffer[i + j * WIDTH] = (ir << 16) | (ig << 8) | ib
+            }
+        }
+        window
+            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .unwrap();
+    }
+}
