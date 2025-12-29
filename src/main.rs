9ead9fbee3aa5d717f199a33e3ddb05332d50c51
janne <jan.valtanen@gmail.com>
2025-12-29 12:19:09 +0100

    walked through the ray casting math

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use glam::Vec3;
 use minifb::{Key, ScaleMode, Window, WindowOptions};
 
 const WIDTH: usize = 1280;
-const HEIGHT: usize = 720;
+const _HEIGHT: usize = 720;
 
 struct Ray {
     origin: Vec3,
@@ -33,12 +33,39 @@ fn vec3_to_u32(color: Vec3) -> u32 {
 }
 
 fn main() {
-    let mut buffer = vec![0u32; WIDTH * HEIGHT];
+    let aspect_ratio = 16.0 / 9.0f32;
+    let image_width: usize = WIDTH;
+
+    let image_height = ((image_width as f32 / aspect_ratio) as usize).max(1);
+
+    let focal_length = 1f32;
+    let viewport_height = 2f32;
+    let viewport_width = viewport_height * (image_width as f32 / image_height as f32);
+    let camera_center = Vec3::ZERO;
+
+    println!(
+        "aspect_ratio: {}, image_width: {}, image_height: {}, focal_length: {}, viewport_height: {}, viewport_width: {}",
+        aspect_ratio, image_width, image_height, focal_length, viewport_height, viewport_width
+    );
+
+    println!("camera_center: {}", camera_center);
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / (image_width as f32);
+    let pixel_delta_v = viewport_v / (image_height as f32);
+
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    let mut buffer = vec![0u32; image_width * image_height];
 
     let mut window = Window::new(
         "first program rtiow",
-        WIDTH,
-        HEIGHT,
+        image_width,
+        image_height,
         WindowOptions {
             resize: false,
             scale_mode: ScaleMode::UpperLeft,
@@ -59,13 +86,12 @@ fn main() {
         if redraw_needed {
             for j in 0..height {
                 for i in 0..width {
-                    let color = Vec3::new(
-                        i as f32 / (width - 1) as f32,
-                        j as f32 / (height - 1) as f32,
-                        0.0,
-                    );
+                    let pixel_center = pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+                    let ray_direction = pixel_center - camera_center;
+                    let r = Ray { origin: camera_center, direction: ray_direction};
 
-                    buffer[i + j * width] = vec3_to_u32(color);
+                    let pixel_color = ray_color(r);
+                    buffer[i + j * width] = vec3_to_u32(pixel_color);
                 }
             }
             redraw_needed = false;
