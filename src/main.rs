9dabf5f2f4519e95dbe457806ca094052b5a0aa2
janne <jan.valtanen@gmail.com>
2026-01-05 19:38:53 +0100

    bubble

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,15 @@ fn main() {
         center: Vec3::new(-1.0, 0.0, -1.0),
         radius: 0.5,
         material: Material::Dielectric(Dielectric {
-            refraction_index: 1.0/1.33,
+            refraction_index: 1.5,
+        }),
+    });
+
+    world.objects.push(Sphere {
+        center: Vec3::new(-1.0, 0.0, -1.0),
+        radius: 0.4,
+        material: Material::Dielectric(Dielectric {
+            refraction_index: 1.0/1.5,
         }),
     });
 
